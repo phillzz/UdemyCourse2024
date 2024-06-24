27d4f7c213976a30849771a6f9cb7b49b9f08ec1
Evgheni Feldman <phillzzzz@gmail.com>
2024-06-24 11:43:48 +0300

    source code init

---
+++ Source/UdemyCourse/BP_UC_GameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BP_UC_GameMode.h"
+
+
+
+
---
+++ Source/UdemyCourse/BP_UC_GameMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "BP_UC_GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UDEMYCOURSE_API ABP_UC_GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
